4cd58fe5c83a6a99ce6e16ea2ebaadfa35ffab82
Mono-94 <marcecanavolt94@gmail.com>
2024-07-25 21:15:09 +0200

    Deploy website - based on

--- assets/js/2b0345ad.9d64a301.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmonodocs=self.webpackChunkmonodocs||[]).push([[2254],{4900:e=>{e.exports=JSON.parse('{"categoryGeneratedIndex":{"title":"mVehicle","description":"Hello","slug":"/category/mvehicle","permalink":"/docs/category/mvehicle","sidebar":"tutorialSidebar","navigation":{"next":{"title":"Guide","permalink":"/docs/mvehicle/"}}}}')}}]);
---
+++ assets/js/acecf23e.7df8aceb.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmonodocs=self.webpackChunkmonodocs||[]).push([[1903],{1912:o=>{o.exports=JSON.parse('{"blogBasePath":"/mDocuments/blog","blogTitle":"Blog"}')}}]);
